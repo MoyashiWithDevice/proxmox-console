00ce0d192ee0c9230f03fabc57b8ad867ad8ff46
Cinnamobot <149035689+Cinnamobot@users.noreply.github.com>
2026-07-17 05:18:44 +0000

    Revert "Remove ip_config dhcp from vm.tf to avoid conflict with cloud-init network config"
    This reverts commit f8b833622a6b242f68b3abfc4a9f15e7f133d79d.

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -28,6 +28,12 @@ resource "proxmox_virtual_environment_vm" "vm" {
   }
 
   initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
     user_data_file_id = proxmox_virtual_environment_file.cloudcfg.id
   }
 

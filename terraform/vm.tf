f8b833622a6b242f68b3abfc4a9f15e7f133d79d
Cinnamobot <149035689+Cinnamobot@users.noreply.github.com>
2026-07-17 04:28:31 +0000

    Remove ip_config dhcp from vm.tf to avoid conflict with cloud-init network config

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -28,12 +28,6 @@ resource "proxmox_virtual_environment_vm" "vm" {
   }
 
   initialization {
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-
     user_data_file_id = proxmox_virtual_environment_file.cloudcfg.id
   }
 
